4180981e6e8ebbfed07eb78123f5f048a2b0d63d
Shashank Naik <shashanknaik808@gmail.com>
2023-06-17 12:26:21 +0530

    Area of Triangle

---
+++ 03basic/Concatenation.java
@@ -0,0 +1,13 @@
+class Concatenation
+{
+  public static void main(String args[])
+
+  {
+     String s1 = "Super";
+     String s2 = "Man";
+     String s3 = "s1 + s2";
+
+     System.out.println("Sum of two strings is: " + s3);
+
+     }
+}
---
+++ 04basic/Area.java
@@ -0,0 +1,16 @@
+class Area
+{
+  public static void main(String args[])
+
+  {
+
+  int base = 22;
+  int height = 33;
+  double areaOfTriangle = 0.5 * base * height;
+
+  System.out.println("Area of a triangle is " + areaOfTriangle
+  + " My name is Batman."
+  + " I have got my output");
+
+  }
+}
