539d9f34ed099f44def5d13d17322a5437ed53ef
Shashank Naik <shashanknaik808@gmail.com>
2023-06-17 10:24:45 +0530

    Second basic Java program

---
+++ 02basic/Addition.java
@@ -0,0 +1,16 @@
+class Addition
+{
+public static void main(String args[])
+{
+
+int a=25;
+int b=35;
+int c=0;
+c=a+b;
+
+System.out.println("Value of a is: " + a);
+System.out.println("Value of b is: " + b);
+System.out.println("Sum of two numbers is: " + c);
+
+}
+}
