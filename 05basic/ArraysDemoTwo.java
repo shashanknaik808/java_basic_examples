d0a4d9fc669196c877aa8ddd888d10f69f11ce62
Shashank Naik <shashanknaik808@gmail.com>
2023-06-19 10:42:14 +0530

    Arrays initializing default

---
+++ 05basic/ArraysDemoTwo.java
@@ -0,0 +1,14 @@
+class ArraysDemoTwo
+{
+
+   public static void main (String args[])
+   {
+   int z[] = new int [5];
+
+   System.out.println("Value in 0th index is : " + z[0]);
+   System.out.println("Value in 1st index is : " + z[1]);
+   System.out.println("Value in 2nd index is : " + z[2]);
+   System.out.println("Value in 3rd index is : " + z[3]);
+   System.out.println("Value in 4th index is : " + z[4]);
+   }
+}
