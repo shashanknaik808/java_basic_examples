99d6606276ff69a323b736ac9ece73271875ce5f
Shashank Naik <shashanknaik808@gmail.com>
2023-06-19 10:50:29 +0530

    Array length program

---
+++ 05basic/ArraysDemoThree.java
@@ -0,0 +1,10 @@
+class ArraysDemoThree
+{
+   public static void main (String[] args)
+   {
+
+      int z[] = new int[5];
+      int arrayLength = z.length;
+      System.out.println("Length of z[] is : " + arrayLength);
+   }
+}
