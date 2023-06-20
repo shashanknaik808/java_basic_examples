df82b0c3d60660f9bbbf3f40e175cadfb44998c7
Shashank Naik <shashanknaik808@gmail.com>
2023-06-20 22:58:44 +0530

    If Statement Program

---
+++ 07basic/ConditionalStatementsDemoOne.java
@@ -0,0 +1,17 @@
+class ConditionalStatementsDemoOne
+{
+  public static void main (String[] args)
+  {
+     int a = 15;
+     int b = 17;
+     int c = 18;
+     int d = 17;
+
+     if (b == d)
+     {
+       System.out.println ("Inside If");
+     }
+
+     System.out.println ("Outside If");
+   }
+}
