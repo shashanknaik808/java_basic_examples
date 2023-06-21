5aa0cf7772c7bb92e5f51e83ad6468fd3efb5606
Shashank Naik <shashanknaik808@gmail.com>
2023-06-22 00:16:37 +0530

    While loop structure

---
+++ 08basic/LoopDemoTwo.java
@@ -0,0 +1,13 @@
+class LoopDemoTwo
+{
+public static void main(String args[])
+{
+int i=5;
+while(i>0)
+{
+System.out.println("My Name is Balaji");
+i++;
+}
+System.out.println("Outside for loop");
+}
+}
