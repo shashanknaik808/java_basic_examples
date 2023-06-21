33730652335421e35c90f9de520010f43e8643da
Shashank Naik <shashanknaik808@gmail.com>
2023-06-22 00:14:44 +0530

    For loop structure

---
+++ 08basic/LoopDemoOne.java
@@ -0,0 +1,11 @@
+class LoopDemoOne
+{
+public static void main(String args[])
+{
+for(int i=0; i<5; i++)
+{
+System.out.println("My Name is Balaji");
+}
+System.out.println("Outside for loop");
+}
+}
