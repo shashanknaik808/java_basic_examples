ba002ba25977bd28e503467c341a9b2513c589d3
Shashank Naik <shashanknaik808@gmail.com>
2023-06-21 23:50:12 +0530

    If else statement program

---
+++ 07basic/ConditionalStatementsDemoTwo.java
@@ -0,0 +1,22 @@
+class ConditionalStatementsDemoTwo
+{
+public static void main(String args[])
+{
+int a = 15;
+int b = 17;
+int c = 18;
+int d = 17;
+
+if(a <=c )
+{
+System.out.println("Inside If");
+}
+else
+{
+System.out.println("Inside else");
+}
+{
+System.out.println("Outside If/else");
+}
+}
+}
