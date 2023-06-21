51afec4f3880dd2168f0b84987d81d1593ec8fa4
Shashank Naik <shashanknaik808@gmail.com>
2023-06-21 23:58:12 +0530

    Switch case program

---
+++ 07basic/ConditionalStatementsDemoFour.java
@@ -0,0 +1,27 @@
+class ConditionalStatementsDemoFour
+{
+public static void main(String args[])
+{
+int p = 1;
+switch(p)
+{
+case1: System.out.println("Inside case 1");
+System.out.println("2nd statement");
+System.out.println("3rd statement");
+break;
+
+case2: System.out.println("Inside case 2");
+break;
+
+case3: System.out.println("Inside case 3");
+break;
+
+case4: System.out.println("Inside case 4");
+break;
+
+default: System.out.println("Inside Default case");
+}
+System.out.println("Outside switch case");
+}
+}
+}
