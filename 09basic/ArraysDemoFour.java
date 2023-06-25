4b5583e172b65eef63aeb8b7f0c8e98f3e515450
Shashank Naik <shashanknaik808@gmail.com>
2023-06-25 19:36:51 +0530

    for loop program

---
+++ 09basic/ArraysDemoFour.java
@@ -0,0 +1,12 @@
+class ArraysDemoFour
+{
+public static void main(String[] args)
+{
+int []z = {43, 75, 89, 116, 267};
+
+for(int i=0; i<z.length; i++)
+{
+System.out.println("Value of z ["+i+"] is:" + z[i]);
+}
+}
+}
