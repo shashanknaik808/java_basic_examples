70c55425666a9d9acc70eefdb7f007eb96b4a0d2
Shashank Naik <shashanknaik808@gmail.com>
2023-06-26 17:04:35 +0530

    Break statement in a switch case

---
+++ 10breakstatement/BreakStatementDemoTwo.java
@@ -0,0 +1,18 @@
+class BreakStatementDemoTwo
+{
+public static void main(String args[])
+{
+int p=2;
+switch(p)
+{
+case 1: System.out.println("Inside case 1");
+break;
+case 2: System.out.println("Inside case 2");
+break;
+case 3: System.out.println("Inside case 3");
+break;
+default:System.out.println("Inside default case");
+}
+System.out.println("Outside switch case");
+}
+}
